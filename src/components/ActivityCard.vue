d7e21ab2642897e49da5d33891709d75e766db85
davidpracilio <dave.pracilio@outlook.com>
2026-02-05 16:41:34 +0800

    Update link logic and text in activity card

--- src/components/ActivityCard.vue
+++ src/components/ActivityCard.vue
@@ -26,7 +26,7 @@
           <svg class="meta-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M10.59 13.41c.41.39.41 1.03 0 1.42-.39.39-1.03.39-1.42 0a5.003 5.003 0 010-7.07l3.54-3.54a5.003 5.003 0 017.07 0 5.003 5.003 0 010 7.07l-1.49 1.49c.01-1.18-.78-2.26-2-2.26-.55 0-1 .45-1 1s.45 1 1 1c.28 0 .53-.11.71-.29l1.49-1.49a3.003 3.003 0 000-4.24 3.003 3.003 0 00-4.24 0l-3.54 3.54a3.003 3.003 0 000 4.24zm2.82-4.24c.39-.39 1.03-.39 1.42 0a5.003 5.003 0 010 7.07l-3.54 3.54a5.003 5.003 0 01-7.07 0 5.003 5.003 0 010-7.07l1.49-1.49c-.01 1.18.78 2.26 2 2.26.55 0 1-.45 1-1s-.45-1-1-1c-.28 0-.53.11-.71.29L8.46 10.7a3.003 3.003 0 000 4.24 3.003 3.003 0 004.24 0l3.54-3.54a3.003 3.003 0 000-4.24.973.973 0 010-1.42z" fill="#4EAFD9"/>
           </svg>
-          <a :href="activity.bookingUrl && !activity.bookingUrl.includes('example.com') ? activity.bookingUrl : `https://www.google.com/search?q=${encodeURIComponent(activity.name)}`" target="_blank" @click.stop class="meta-text">{{ activity.bookingUrl && !activity.bookingUrl.includes('example.com') ? 'Visit' : 'Search' }}</a>
+          <a :href="activity.bookingUrl && !activity.bookingUrl.includes('example.com') && activity.bookingUrl !== 'Not available' ? activity.bookingUrl : `https://www.google.com/search?q=${encodeURIComponent(activity.name)}`" target="_blank" @click.stop class="meta-text">{{ activity.bookingUrl && !activity.bookingUrl.includes('example.com') && activity.bookingUrl !== 'Not available' ? 'Visit' : 'See Results' }}</a>
         </span>
       </div>
     </div>
